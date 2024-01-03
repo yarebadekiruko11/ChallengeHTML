4e517617952f2f124c810f67aba13043c289c097
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2024-01-03 13:48:05 +0000

    Java６章イベント終了

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,9 +1,8 @@
 $(function(){
-  $('.box1').slideDown('slow', function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'height': '100px',
-      'width': '200px'
-    }).slideUp(2000);
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
