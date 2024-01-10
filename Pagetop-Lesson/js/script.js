1e2e69cbe6660eccf7047e589949a773f28baa10
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2024-01-10 14:58:38 +0000

    9章終了

--- Pagetop-Lesson/js/script.js
+++ Pagetop-Lesson/js/script.js
@@ -1,13 +1,8 @@
-const swiper = new Swiper('.swiper', {
-  // オプションの設定
-  loop: true,
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    },800);
+    event.preventDefault();
+  });
 });
